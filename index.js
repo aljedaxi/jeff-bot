c5d699f400c9ced099e8bbcddcdf4a776d9aad2b
jacob whitford-bender <jacob@xmentium.com>
2021-07-02 22:35:09 -0600

    fix: think that might fix it

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const pressF = sender => o => sender (`something went wrong: ${JSON.stringify (o
 const message = ({client}) => message => {
 	if (message.author.bot) return
 	Object.entries(commands).some(([k,v]) => {
-		new RegExp (k, 'ig').test(message.content) && v ({client, message}).catch(pressF (send (message))) && true
+		new RegExp (k, 'ig').test(message.content) && v ({client, message}).catch(pressF (send (message))), true
 	})
 }
 
